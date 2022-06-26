1eabfdc63ab4e335d7f71933869c705d186c6951
djiby ba <djibyba477@gmail.com>
2022-06-26 06:22:45 +0000

    ma premiere commit

---
+++ database/migrations/2022_06_26_061937_create_etudiants_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEtudiantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('etudiants', function (Blueprint $table) {
+            $table->id();
+            $table->string("matricule");
+            $table->string("nomComplet");
+            $table->string("telephone");
+            $table->string("adresse");
+            $table->string("statut")->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('etudiants');
+    }
+}
